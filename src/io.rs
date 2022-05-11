7f36d67bcace6264538b55d98a0e8923806a82d9
orifuji <orfj18@gmail.com>
2022-05-11 23:58:22 +0900

    feat(io): とりあえずコマンドライン引数を受け取れるようにした

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,5 @@
+use clap::{Arg, Command};
+
 pub struct CliArgs {
     pub number_of_dice: usize,
     pub max: usize,
@@ -5,10 +7,16 @@ pub struct CliArgs {
 }
 
 pub fn input() -> CliArgs {
+    let matches = self::create_command().get_matches();
+    let arg1 = matches.value_of("number").unwrap();
+    let number_of_dice = arg1.parse::<usize>().unwrap();
+    let arg2 = matches.value_of("max").unwrap();
+    let max = arg2.parse::<usize>().unwrap();
+
     return CliArgs {
-        number_of_dice: 3,
-        max: 3,
-        min: 0,
+        number_of_dice: number_of_dice,
+        max: max,
+        min: 1,
     };
 }
 
@@ -18,4 +26,24 @@ pub fn output(args: CliArgs, result: usize) {
 
     let statement = dice + " -> " + result_as_string;
     println!("{}", statement);
+}
+
+// FIXME: 2d100みたいな感じで受け取ってパースしたい
+fn create_command() -> Command<'static> {
+    return Command::new("trpgd")
+        .version("1.0")
+        .author("orifuji")
+        .about("Does awesome things")
+        .arg(
+            Arg::new("number")
+                .required(true)
+                .help("set your number of dice to roll")
+                .index(1),
+        )
+        .arg(
+            Arg::new("max")
+                .required(true)
+                .help("set max value of your dice")
+                .index(2),
+        );
 }
