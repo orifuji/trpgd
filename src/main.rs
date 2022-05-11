5ddec8b7091991428ec0fa38586b1a8bf811d278
orifuji <orfj18@gmail.com>
2022-05-11 23:56:42 +0900

    fix(main): rollについて、minを動的に渡すように修正した

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use crate::io as cli_io;
 
 fn main() {
     let inputs = cli_io::input();
-    let result = dice::roll(inputs.number_of_dice, inputs.max, 0);
+    let result = dice::roll(inputs.number_of_dice, inputs.max, inputs.min);
     cli_io::output(inputs, result);
 }
